b71214028213bdb6a02fe7324a4518a9e02571b4
parahan <sena.m.no1@gmail.com>
2026-08-19 23:39:30 +0900

    Add 2 pattern

--- src/main/java/com/example/identicon/Main.java
+++ src/main/java/com/example/identicon/Main.java
@@ -32,27 +32,55 @@ public final class Main {
         String panelBinary = Integer.toBinaryString(panel);
         System.out.println(panelBinary);
 
-        boolean icon[][] = new boolean[7][7];
+        boolean iconA[][] = new boolean[7][7];
+        boolean iconB[][] = new boolean[7][7];
 
-        int panelCount;
-        int shiftPanel = panel;
-        for (int i = icon.length - 1; 3 <= i; i--) {
-            for (int j = icon[0].length - 1; 0 <= j; j--) {
-                panelCount = shiftPanel & 1;
-                if (panelCount == 1) {
-                    icon[j][i] = true;
+        int panelCountA;
+        int shiftPanelA = panel;
+        for (int i = iconA.length - 1; 3 <= i; i--) {
+            for (int j = iconA[0].length - 1; 0 <= j; j--) {
+                panelCountA = shiftPanelA & 1;
+                if (panelCountA == 1) {
+                    iconA[j][i] = true;
                 }
-                shiftPanel = shiftPanel >>> 1;
+                shiftPanelA = shiftPanelA >>> 1;
             }
         }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 7; j++) {
-                icon[j][i] = icon[j][6 - i];
+                iconA[j][i] = iconA[j][6 - i];
+            }
+        }
+        int panelCountB;
+        int shiftPanelB = panel;
+        for (int i = iconB.length - 1; 0 <= i; i--) {
+            for (int j = iconB[0].length - 1; 3 <= j; j--) {
+                panelCountB = shiftPanelB & 1;
+                if (panelCountB == 1) {
+                    iconB[i][j] = true;
+                }
+                shiftPanelB = shiftPanelB >>> 1;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                iconB[j][i] = iconB[j][6 - i];
             }
         }
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 7; j++) {
-                if (icon[i][j]) {
+                if (iconA[i][j]) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (iconB[i][j]) {
                     System.out.print(1);
                 } else {
                     System.out.print(0);
