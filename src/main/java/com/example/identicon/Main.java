461dab0efc84f93c90a19b0c36d7d9eb043db160
parahan <202758590+paraparachahan@users.noreply.github.com>
2026-08-23 19:50:36 +0900

    Update convert to constant

--- src/main/java/com/example/identicon/Main.java
+++ src/main/java/com/example/identicon/Main.java
@@ -15,9 +15,32 @@ public final class Main {
     private Main() {
     }
 
+    private static final Color[] COLORS = {
+            new Color(0xD64545),
+            new Color(0xE06B35),
+            new Color(0xD49A28),
+            new Color(0xA6A832),
+            new Color(0x5C9E45),
+            new Color(0x3A9B72),
+            new Color(0x329C9C),
+            new Color(0x3D8DBD),
+            new Color(0x4B72C2),
+            new Color(0x6258B5),
+            new Color(0x7A53B8),
+            new Color(0x9950A5),
+            new Color(0xB84F82),
+            new Color(0xC95D68),
+            new Color(0x6F7D8C),
+            new Color(0x5F6B62)
+    };
+
+    private static final int ICON_SIZE = 7;
+    private static final int CELL_SIZE = 40;
+    private static final int PADDING = 1;
+
     public static void saveImage(boolean[][] icon, Color iconColor, String imageName) throws IOException {
-        int cellSize = 40;
-        int imageSize = 9 * cellSize;
+
+        int imageSize = (ICON_SIZE + 2 * PADDING) * CELL_SIZE;
 
         BufferedImage image = new BufferedImage(imageSize, imageSize, BufferedImage.TYPE_INT_ARGB);
 
@@ -25,10 +48,10 @@ public final class Main {
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, imageSize, imageSize);
         g.setColor(iconColor);
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
+        for (int i = 0; i < ICON_SIZE; i++) {
+            for (int j = 0; j < ICON_SIZE; j++) {
                 if (icon[i][j]) {
-                    g.fillRect((j + 1) * cellSize, (i + 1) * cellSize, cellSize, cellSize);
+                    g.fillRect((j + 1) * CELL_SIZE, (i + 1) * CELL_SIZE, CELL_SIZE, CELL_SIZE);
                 }
             }
         }
@@ -48,11 +71,11 @@ public final class Main {
     }
 
     public static boolean[][] generateIcon(int panel, int pattern) {
-        boolean[][] icon = new boolean[7][7];
+        boolean[][] icon = new boolean[ICON_SIZE][ICON_SIZE];
         int panelBit;
         int shiftPanel = panel;
         if (pattern == 1) {
-            for (int i = icon.length - 1; 3 <= i; i--) {
+            for (int i = icon.length - 1; (ICON_SIZE / 2) <= i; i--) {
                 for (int j = icon[0].length - 1; 0 <= j; j--) {
                     panelBit = shiftPanel & 1;
                     if (panelBit == 1) {
@@ -63,7 +86,7 @@ public final class Main {
             }
         } else {
             for (int i = icon.length - 1; 0 <= i; i--) {
-                for (int j = icon[0].length - 1; 3 <= j; j--) {
+                for (int j = icon[0].length - 1; (ICON_SIZE / 2) <= j; j--) {
                     panelBit = shiftPanel & 1;
                     if (panelBit == 1) {
                         icon[i][j] = true;
@@ -97,26 +120,7 @@ public final class Main {
         boolean[][] iconA = generateIcon(panel, 1);
         boolean[][] iconB = generateIcon(panel, 2);
 
-        Color[] colors = {
-                new Color(0xD64545),
-                new Color(0xE06B35),
-                new Color(0xD49A28),
-                new Color(0xA6A832),
-                new Color(0x5C9E45),
-                new Color(0x3A9B72),
-                new Color(0x329C9C),
-                new Color(0x3D8DBD),
-                new Color(0x4B72C2),
-                new Color(0x6258B5),
-                new Color(0x7A53B8),
-                new Color(0x9950A5),
-                new Color(0xB84F82),
-                new Color(0xC95D68),
-                new Color(0x6F7D8C),
-                new Color(0x5F6B62)
-        };
-
-        Color iconColor = colors[color];
+        Color iconColor = COLORS[color];
 
         saveImage(iconA, iconColor, "iconA.png");
         saveImage(iconB, iconColor, "iconB.png");
